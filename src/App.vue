72049ee91271349f2bf5d303d5d3691d9257cf5e
Nik <nik.moiseienko@gmail.com>
2026-03-27 18:20:30 +0100

    feat: ... [submodule:hft_web]

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+import { User, UserId } from "./domain/auth/User";
+import { FormKit } from "@formkit/vue";
+
+const user = User({ id: UserId(crypto.randomUUID()) });
 </script>
 
-<template>hello world</template>
+<template>
+  <FormKit type="button" label="Sign In with Google"></FormKit>
+</template>
